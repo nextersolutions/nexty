1d15fe63d61c15756ba8518775605940cd3b1fc7
Andrii Dubovyk <dubovyk@weelorum.com>
2025-10-02 18:08:07 +0300

    init

---
+++ nexty/src/main/java/com/nextersolutions/nexty/Nexty.kt
@@ -0,0 +1,41 @@
+package com.nextersolutions.nexty
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+object Nexty {
+    private val pairs = mutableMapOf<String, Any?>()
+    private val mutablePairs = mutableMapOf<String, MutableStateFlow<Any?>>()
+
+    fun put(key: String, value: Any?) {
+        pairs[key] = value
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> get(key: String): T? {
+        val value = pairs.getOrDefault(key, null)
+        return try {
+            value as? T
+        } catch (_: Exception) {
+            null
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun putMutable(key: String, value: Any?): Flow<Any?> {
+        var old = mutablePairs.getOrDefault(key, null)
+        if (old == null) {
+            old = MutableStateFlow(value)
+            mutablePairs[key] = old
+        } else {
+            old.value = value
+        }
+
+        return old.asSharedFlow()
+    }
+
+    fun getAsFlow(key: String): Flow<Any?>? {
+        return mutablePairs.getOrDefault(key, null)?.asSharedFlow()
+    }
+}
